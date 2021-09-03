3eeffecbb6a7d3668236cfd8ebd37a0788560217
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-09-03 11:07:28 +0530

    Update config.js

--- Configs/config.js
+++ Configs/config.js
@@ -5,5 +5,11 @@ module.exports = {
     embedcolor: 'white',
     database: process.env.MONGO,
     FACT_API: 'c9e770b9cc2c235b94f4ee257939e28679fff39d2f9a',
-    AME_API: 'c9e770b9cc2c235b94f4ee257939e28679fff39d2f9a'
+    AME_API: '8489d35e68f039a5a904c911789c0ca7aa2ac348f0153bf21701aac7b4cb830696a51af4e6d0a66656dc309004fc0a325565b49589098c562f1d7ae1e426c05f',
+    status : 'online',
+    activit': ' .help | MADE BY ALBIN PRAVEEN',
+    type: 'LISTENING', 
+    url: 'https://albinpraveen.ml',
+    bid: '156561',
+    key: '590152.ow7FhyLazz0gVNLq'
 } 

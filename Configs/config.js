af83e2c09b02b8bcd14de729036c588963b214c1
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-09-19 18:51:18 +0530

    Update config.js

--- Configs/config.js
+++ Configs/config.js
@@ -3,7 +3,7 @@ module.exports = {
     prefix: process.env.PREFIX,
     devs: process.env.DEVS,
     embedcolor: 'white',
-    database: '',
+    database: 'mongodb+srv://Albinpraveen:ALBINPRAVEEN@cluster0.mmobp.mongodb.net/Cluster0?retryWrites=true&w=majority',
     FACT_API: 'c9e770b9cc2c235b94f4ee257939e28679fff39d2f9a',
     AME_API: '8489d35e68f039a5a904c911789c0ca7aa2ac348f0153bf21701aac7b4cb830696a51af4e6d0a66656dc309004fc0a325565b49589098c562f1d7ae1e426c05f',
     status : 'online',
